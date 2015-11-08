bd15f840fc61ebfebb5b981ef4624cbc26279502
DC00 <djc4ku@virginia.edu>
2015-11-07 21:04:55 -0500

    working report links and directory setup

--- reports/views.py
+++ reports/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from django.shortcuts import render
+
 from .models import Report
 
 def index(request):
     latest_report_list = Report.objects.order_by('-created_at')
-    output = ""
-    for r in latest_report_list.reverse():
-        output += str(r.id) + " " + r.description + "<br>"
-    # output = '<br>'.join([r.description for r in latest_report_list])
-    return HttpResponse(output)
+    
+    # Loads the template at reports/index.html and passes it a context
+    # the context is a dictionary mapping template variable names to Python objects
+    # e.g. maps 'latest_report_list' -> latest_report_list
+    context = {'latest_report_list': latest_report_list}
+    return render(request, 'reports/index.html', context)
 
 def detail(request, report_id):
     return HttpResponse("You're looking at report %s." % report_id)
