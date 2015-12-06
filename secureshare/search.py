ad5edb3c7c587b8aa814c37efaea2bbf6bd6a82e
DC00 <djc4ku@virginia.edu>
2015-12-06 12:51:52 -0500

    search v2.0

--- secureshare/search.py
+++ secureshare/search.py
@@ -1,84 +1,118 @@
-from secureshare.models import Report
-
-import re
-
 from whoosh.query import Query
 from whoosh.index import create_in
 from whoosh.fields import *
-from whoosh.qparser import QueryParser, OperatorsPlugin
+from whoosh.qparser import QueryParser, MultifieldParser, OperatorsPlugin
 from whoosh.index import open_dir
 from whoosh import scoring
 
+from secureshare.models import Report
 
-def make_search_index():
-    schema = Schema(description=TEXT(stored=True), full_description=TEXT(stored=True), report_id=NUMERIC(stored=True, numtype=int))
-    ix = create_in("indexdir", schema)
-    writer = ix.writer()
-    rlist = Report.objects.all()
-    for r in rlist:
-        writer.add_document(description=r.description, full_description=r.full_description, report_id=r.id)
-    writer.commit()
+from indexer import *
 
-def ranksearch(function, query):
+
+def ranked_search(query):
     # ix = create_in("indexdir", schema)
     ix = open_dir("indexdir")
     writer = ix.writer()
+    with ix.searcher(weighting=scoring.BM25F(0.75, 1.2)) as searcher:
+        # qp = QueryParser("full_description", schema)
+        qp = MultifieldParser(["description", "full_description", "file_text", "reporter", "date"], ix.schema)
+        cp = OperatorsPlugin(And="&", Or="\|", AndMaybe="&~", Not=None)
+        qp.replace_plugin(cp)
+
+        qAND = qp.parse(query, normalize=True)
+        qOR = qp.parse(queryOR(query), normalize=True)
+        qAM = qp.parse(queryAndMaybe(query), normalize=True)
+        
+        # results are Result objects which have lists of hits
+        qAND_docs = searcher.search(qAND, limit=None, terms=True)
+        qOR_docs = searcher.search(qOR, limit=None, terms=True)
+        qAM_docs = searcher.search(qAM, limit=None, terms=True)
+
+        qAND_docs.fragmenter.maxchars = 160
+        qOR_docs.fragmenter.maxchars = 160
+        qAM_docs.fragmenter.maxchars = 160
+
+        # Shortening the surrounding chars of text. Default was 20. Now tweet size
+        qAND_docs.fragmenter.surround = 20
+        qOR_docs.fragmenter.surround = 20
+        qAM_docs.fragmenter.surround = 20
 
 
-    # {key=report_id, value=score}
-    results = []
+        # Used for highlighting matched terms
+        # Scoring will still be reflected in order.
+        # Sending only OR docs will just simplify display
+        hits = get_stats(qOR_docs)
 
+        results = []
 
-    if function is 'bm25':
-        with ix.searcher(weighting=scoring.BM25F(0.75, 1.2)) as s:
-            qp = QueryParser("full_description", ix.schema)
-            cp = OperatorsPlugin(And="&", Or="\|", AndMaybe="&~", Not=None)
-            qp.replace_plugin(cp)
-
-            q = qp.parse(query, normalize=True)
-
+        for i in range(len(list(qAND_docs))):
+            report_id = Report.objects.get(pk=int(qOR_docs[i].fields()['report_id']))
+            results.append((report_id, qOR_docs[i].score))
             
 
+        for j in range(len(list(qOR_docs))):
+            report_id = Report.objects.get(pk=int(qOR_docs[j].fields()['report_id']))
+            results.append((report_id, qOR_docs[j].score))
 
-            # Make tuple list of (score, report) for AND query
-            hits = s.search(q, limit=None, terms=True)
-            for inx in range(len(list(hits))):
-                report_id = int(hits[inx].fields()['report_id'])
-                report = Report.objects.get(pk=report_id)
-                results.append((hits[inx].score, report))
-                
-            
-            # Make tuple list of (score, report) for OR query
-            qOR = qp.parse(queryOR(query), normalize=True)
-            hits2 = s.search(qOR, limit=None, terms=True)
-            for inx2 in range(len(list(hits2))):
-                report = Report.objects.get(pk=int(hits2[inx2].fields()['report_id']))
-                results.append((hits2[inx2].score, report))
-            
-            # Make tuple list of (score, report) for AndMaybe query
-            qAM = qp.parse(queryAndMaybe(query), normalize=True)
-            hits3 = s.search(qAM, limit=None, terms=True)
-            for inx3 in range(len(list(hits3))):
-                report = Report.objects.get(pk=int(hits3[inx3].fields()['report_id']))
-                results.append((hits3[inx3].score, report))
-
-                
-            # Sort by score across expanded query results: AND, OR, AndMaybe
-            sorted_by_score = sorted(results, key=lambda tup: tup[0])
-            ranked_results = []
-            for i, j in results:
-                if j not in results:
-                    ranked_results.append(j)
-
-            return ranked_results
+        for k in range(len(list(qAM_docs))):
+            report_id = Report.objects.get(pk=int(qAM_docs[k].fields()['report_id']))
+            results.append((report_id, qAM_docs[k].score))
 
 
-    # Next time lol
-    # elif function is 'tfidf':
-    #     with ix.searcher(weighting=scoring.TF_IDF) as s:
-    #         q = QueryParser("full_description", ix.schema).parse(query)
-    #         hits = s.search(q, limit=None)
-            
+        results = removeDuplicates(sorted(results,key=lambda x: x[0], reverse=True))
+
+        
+        results_and_info = { 'results' : results,
+                              'hits' : hits,
+                        }
+
+        return results_and_info
+        
+
+
+def get_stats(or_docs):
+    # report_id : stats
+    hits = {}
+    matched_terms = []
+    for a, b in or_docs.matched_terms():
+        matched_terms.append(b)
+
+    for h in or_docs:
+        r_id = h.fields()['report_id']
+        hits[r_id] = {}
+        hits[r_id]['description'] = set()
+        hits[r_id]['full_description'] = set()
+        hits[r_id]['file_text'] = set()
+        hits[r_id]['reporter'] = set()
+        hits[r_id]['date'] = set()
+
+
+        for term in matched_terms:
+            if term.lower() in h.highlights('description', top=5).lower():
+                hits[r_id]['description'].add(term)
+
+            if term.lower() in h.highlights('full_description', top=5).lower():
+                hits[r_id]['full_description'].add(term)
+
+            if term.lower() in h.highlights('file_text', top=5).lower():
+                hits[r_id]['file_text'].add(term)
+
+            if term.lower() in h.highlights('reporter', top=5).lower():
+                hits[r_id]['reporter'].add(term)
+
+            if term.lower() in h.highlights('date', top=5).lower():
+                hits[r_id]['date'].add(term)
+
+    return hits
+
+
+
+def removeDuplicates(x):
+    for i in reversed(range(len(x))):
+        if x[i][0] == x[i-1][0] and i!=0:
+            x.pop(i)
+    return x
 
 
 # Query Expansion. Generate OR and AndMaybe queries
@@ -115,15 +149,5 @@ def queryAndMaybe(query):
     return query_AndMaybe
 
 
-# Remove indexdir from the cmd line if not working and remake
-# make_search_index()
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    print("running locally")
